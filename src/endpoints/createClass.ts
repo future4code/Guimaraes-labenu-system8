31db5e0c07d60a7f4e17f644073cc2cc5c206ba6
Jenifer <jeniferkindermann@gmail.com>
2022-07-19 19:12:19 -0300

    Projeto labenu-system no ar

---
+++ src/endpoints/createClass.ts
@@ -0,0 +1,16 @@
+import {v4 as uuid} from "uuid"
+
+class Class {
+    id: string;
+    name: string;
+    module: string;
+
+    constructor(id: string, name: string, module: string){
+        this.id = id;
+        this.name = name;
+        this.module = module
+    }
+
+
+
+}
